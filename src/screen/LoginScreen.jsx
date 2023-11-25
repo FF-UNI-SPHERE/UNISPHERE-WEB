ede37382954f8928d0c47e101238770108febaf1
StopSoo <jpy1030@naver.com>
2023-11-25 12:57:54 +0900

    <refactor>: 변경사항 적용

---
+++ src/screen/LoginScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginTemplate from "../component/login/LoginTemplate";
+
+function LoginScreen() {
+    // 여기서 뭔가 url이나 요청 보내는걸로
+    return (
+        <LoginTemplate />
+    );
+}
+
+export default LoginScreen;
