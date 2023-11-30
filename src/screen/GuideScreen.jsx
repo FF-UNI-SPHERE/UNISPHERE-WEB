45cd19ff97e79df69088220573c8745a7186dde3
StopSoo <jpy1030@naver.com>
2023-11-30 11:12:45 +0900

    <chore>: .gitignore 등 외 기타 파일 변경 사항 적용

---
+++ src/screen/GuideScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GuideTemplate from "../component/guide/GuideTemplate";
+
+function GuideScreen() {
+    return (
+        <GuideTemplate />
+    );
+};
+
+export default GuideScreen;
